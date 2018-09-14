f492ff3ccadc8138162c998992971d5e5768fda1
Jacob Abbott <jaa661@nyu.edu>
2018-09-14 13:10:24 -0400

    OG commit
    its just to check

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("boy oh boy");
+    }
+
+}
