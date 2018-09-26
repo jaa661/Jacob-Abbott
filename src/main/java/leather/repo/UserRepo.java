bba836d7a3361f61341f961e355c18271005e5ce
Jacob Abbott <jaa661@nyu.edu>
2018-09-26 11:13:02 -0400

    minor updates
    port update as well

--- src/main/java/leather/repo/UserRepo.java
+++ src/main/java/leather/repo/UserRepo.java
@@ -1,11 +1,11 @@
 package leather.repo;
 
 import org.springframework.data.repository.CrudRepository;
-import leather.entity.User;
+import leather.entity.Users;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepo extends CrudRepository<User, Integer> {
+public interface UserRepo extends CrudRepository<Users, Integer> {
 
 }
