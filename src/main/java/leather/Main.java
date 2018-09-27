a268bb40081694b9e9c56a0935ed406f20bea786
Jacob Abbott <jaa661@nyu.edu>
2018-09-26 23:06:48 -0400

    added servlet handle in main
    servlet issue i guess

--- src/main/java/leather/Main.java
+++ src/main/java/leather/Main.java
@@ -2,15 +2,21 @@ package leather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         System.out.println("running");
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
 }
